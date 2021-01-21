5d9cc70b0c615990ebbbb89fb67e54099e91f5d7
zeeshan ahmad <zeeshan.ahmad@zaxiss.com>
2021-01-21 12:05:36 +0500

    Added echo text

--- app.js
+++ app.js
@@ -5,22 +5,54 @@ const app = express();
 app.use(express.static('public'));
 app.use(express.json());
 
-app.post('/call', function(req, res) {
-    console.log('body =',req.body);
-res.send('response from node.js');
+app.post('/call', function (req, res) {
+    console.log('body =', req.body);
+    res.send('response from node.js');
 });
-// app.use('/call/create', callController);
-// app.use('/call/start', callController);
-// app.use('/call/stop', callController);
-// app.use('/call/session', callController);
-// app.use('/call/sendEmail', callController);
-// app.use('/create', callController);
-app.listen(process.env.PORT || 5000, () => {
-    console.log("---------------------------------------------------------");
-    console.log(" ")
-    console.log(` Server is listening on port ${SERVER_PORT}`);
-    console.log(" ")
-    console.log("---------------------------------------------------------");
-    
-    
-});
+app.post("/echo", function (req, res) {
+    var speech =
+        req.body.queryResult &&
+            req.body.queryResult.parameters &&
+            req.body.queryResult.parameters.echoText
+            ? req.body.queryResult.parameters.echoText
+            : "Seems like some problem. Speak again.";
+
+    var speechResponse = {
+        google: {
+            expectUserResponse: true,
+            richResponse: {
+                items: [
+                    {
+                        simpleResponse: {
+                            textToSpeech: speech
+                        }
+                    }
+                ]
+            }
+        }
+    };
+    return res.json({
+        payload: speechResponse,
+        //data: speechResponse,
+        fulfillmentText: speech,
+        speech: speech,
+        displayText: speech,
+        source: "webhook-echo-sample"
+      });
+    });
+
+    // app.use('/call/create', callController);
+    // app.use('/call/start', callController);
+    // app.use('/call/stop', callController);
+    // app.use('/call/session', callController);
+    // app.use('/call/sendEmail', callController);
+    // app.use('/create', callController);
+    app.listen(process.env.PORT || 5000, () => {
+        console.log("---------------------------------------------------------");
+        console.log(" ")
+        console.log(` Server is listening on port ${SERVER_PORT}`);
+        console.log(" ")
+        console.log("---------------------------------------------------------");
+
+
+    });
